bbc3ecc60f8292a6fdb880ffec88d8ccf5dc0da5
XTStarry <775750806@qq.com>
2018-11-07 21:13:02 +0800

    加入了流程模板

--- Assets/GameMain/Scripts/Procedure/ProcedureMenu.cs
+++ Assets/GameMain/Scripts/Procedure/ProcedureMenu.cs
@@ -18,14 +18,33 @@ using ProcedureOwner = GameFramework.Fsm.IFsm<GameFramework.Procedure.IProcedure
 public class ProcedureMenu : ProcedureBase
 {
 
+    // 游戏初始化时执行。
+    protected override void OnInit(ProcedureOwner procedureOwner)
+    {
+        base.OnInit(procedureOwner);
+    }
+
+    // 每次进入这个流程时执行。
     protected override void OnEnter(ProcedureOwner procedureOwner)
     {
         base.OnEnter(procedureOwner);
     }
 
-
+    // 每次轮询执行。
     protected override void OnUpdate(ProcedureOwner procedureOwner, float elapseSeconds, float realElapseSeconds)
     {
         base.OnUpdate(procedureOwner, elapseSeconds, realElapseSeconds);
+    }
+
+    // 每次离开这个流程时执行。
+    protected override void OnLeave(ProcedureOwner procedureOwner, bool isShutdown)
+    {
+        base.OnLeave(procedureOwner, isShutdown);
+    }
+
+    // 游戏退出时执行。
+    protected override void OnDestroy(ProcedureOwner procedureOwner)
+    {
+        base.OnDestroy(procedureOwner);
     }
 }
